01bc644c60e51783d9576b7315a80699eda1d0e2
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2019-12-23 23:09:18 +0100

    Upload code for SampleDotNetWebService project.

---
+++ SampleDotNetWebServiceClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDotNetWebServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.Service1 client = new localhost.Service1();
+            bool response = client.AuthenticateUser("admin", "admin");
+            Console.WriteLine(response);
+        }
+    }
+}
